4f7421ba40cf93fd7a1526e329919d84b3b9bb5c
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-05-01 21:34:09 +0900

    🚧

---
+++ fish/config/abbrs_aliases.fish
@@ -0,0 +1,78 @@
+alias vim nvim
+abbr -a v nvim
+abbr -a nv nvim
+abbr -a bash 'bash --norc'
+abbr ll 'ls -hl'
+abbr la 'ls -hlA'
+abbr lt 'ls --tree'
+abbr lg 'ls -hlFg'
+abbr -a cdr 'cd (git root)'
+abbr -a sed gsed
+abbr -a sc "source $FISH_CONFIG"
+abbr -a br brew
+abbr -a t tmux
+abbr -a tt tmuximum
+abbr -a wt wezterm
+abbr -a bri 'brew install'
+abbr -a clr clear
+abbr -a rr 'rm -r'
+abbr -a rf 'rm -rf'
+abbr -a mkd 'mkdir -p'
+abbr -a mkdir 'mkdir -p'
+abbr -a src source
+abbr -a cdd __fzf_cd
+abbr -a o open
+abbr -a bunb 'bun --bun'
+abbr -a bunbx 'bunx --bun'
+abbr -a cda conda
+abbr -a lzg lazygit
+abbr -a lzd lazydocker
+abbr -a ech envchain
+abbr -a vc 'code (pwd)'
+abbr -a jn 'jupyter notebook'
+abbr -a jl 'jupyter lab'
+abbr -a py python
+abbr -a d docker
+abbr -a dp "docker ps"
+abbr -a db "docker build"
+abbr -a dr "docker run --rm"
+abbr -a dx "docker exec -it"
+abbr -a dc docker compose
+abbr -a dcu "docker compose up"
+abbr -a dcub "docker compose up --build"
+abbr -a dcd "docker compose down"
+abbr -a dcr "docker compose restart"
+
+abbr -a der "deno run -A --unstable"
+
+abbr -a cpf "pbcopy < "
+abbr -a paf "pbpaste > "
+
+# git configs
+type -q bit && alias git bit
+abbr -a g git
+abbr -a ga 'git add'
+abbr -a ga. 'git add .'
+abbr -a gaa 'git add --all'
+abbr -a gco 'git checkout'
+abbr -a gc 'git commit'
+abbr -a gcn 'git commit -n'
+abbr -a gcl 'git clone'
+abbr -a --set-cursor gcm git commit -m \"%\"
+abbr -a --set-cursor gcnm git commit -n -m \"%\"
+abbr -a --set-cursor gcam git commit --amend -m \"%\"
+abbr -a gcz 'git cz'
+abbr -a gcza 'git cza'
+abbr -a gp 'git push'
+abbr -a gpo 'git push origin'
+abbr -a gpf 'git pushf'
+abbr -a gpfo 'git pushf origin'
+abbr -a gpl 'git pull'
+abbr -a gf 'git fetch'
+abbr -a gsw 'git switch'
+abbr -a gg 'ghq get'
+
+# github copilot
+abbr -a --set-cursor q gh copilot suggest -t shell \"%\"
+abbr -a --set-cursor qgit gh copilot suggest -t git \"%\"
+abbr -a --set-cursor qgh gh copilot suggest -t gh \"%\"
