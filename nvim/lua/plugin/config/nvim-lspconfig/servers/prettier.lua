42ec73916052ed63cc017499c1d223b967a5bef1
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-09-24 20:43:19 +0900

    ✨ setting prettier

---
+++ nvim/lua/plugin/config/nvim-lspconfig/servers/prettier.lua
@@ -0,0 +1,26 @@
+local lsp_utils = require("plugin.config.nvim-lspconfig.utils")
+local ft = lsp_utils.ft
+local setup = lsp_utils.setup
+local format_config = lsp_utils.format_config
+
+local filetype = ft.js_framework_like
+
+---@type LazySpec
+return {
+	name = "prettier",
+	dir = "",
+	dependencies = {
+    "cli",
+		"node_servers",
+		"neovim/nvim-lspconfig",
+	},
+	ft = filetype,
+	opts = function()
+		return {
+			on_attach = format_config(true),
+		}
+	end,
+	config = function(spec, opts)
+		setup(spec.name, opts)
+	end,
+}
